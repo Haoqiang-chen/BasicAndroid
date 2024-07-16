f42efbdad077854c6e74a62c3ebf50f77da3667b
chenhaoqiang <chenhaoqiang@baidu.com>
2024-07-16 21:24:59 +0800

    数据库示例

---
+++ app/src/main/java/cc/aiknow/basicandroid/androidviewpager/ViewPager2Activity.kt
@@ -0,0 +1,21 @@
+package cc.aiknow.basicandroid.androidviewpager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cc.aiknow.basicandroid.R
+
+class ViewPager2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_pager2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
